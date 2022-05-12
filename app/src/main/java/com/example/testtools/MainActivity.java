f8f3b3806d84b06c5f37cb00005fb9c7403e2be6
duqianlong <15313592362@163.com>
2022-05-12 22:48:06 +0800

    test：首次提交

---
+++ app/src/main/java/com/example/testtools/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.testtools;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTv;
+    private int num=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViewe();
+    }
+
+    private void initViewe() {
+        mTv=findViewById(R.id.mTv);
+        mTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTv.setText("啦啦啦啊"+(++num));
+                Log.e("Mainactivity",num+"");
+            }
+        });
+
+    }
+}
